042c041a4d6af698306b1997f4cb2dc8ac0e7e76
only <742122778@qq.com>
2018-06-29 12:49:42 +0800

    提交数据库结构及数据

---
+++ 数据库/generalcourse.sql
@@ -0,0 +1,195 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : root
+ Source Server Type    : MySQL
+ Source Server Version : 50720
+ Source Host           : localhost:3306
+ Source Schema         : generalcourse
+
+ Target Server Type    : MySQL
+ Target Server Version : 50720
+ File Encoding         : 65001
+
+ Date: 29/06/2018 12:46:37
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for article
+-- ----------------------------
+DROP TABLE IF EXISTS `article`;
+CREATE TABLE `article`  (
+  `id` char(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `title` char(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `author` char(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `content` char(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `createTime` datetime(0) NULL DEFAULT NULL,
+  `columnID` char(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `whetherTop` int(11) NULL DEFAULT NULL,
+  `status` int(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for columns
+-- ----------------------------
+DROP TABLE IF EXISTS `columns`;
+CREATE TABLE `columns`  (
+  `id` char(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `name` char(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `level` int(11) NULL DEFAULT NULL,
+  `parent` char(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of columns
+-- ----------------------------
+INSERT INTO `columns` VALUES ('15302448123469110721', '首页', 1, NULL);
+INSERT INTO `columns` VALUES ('15302450997091385528', '课程介绍', 1, NULL);
+INSERT INTO `columns` VALUES ('15302451002015481731', '教学团队', 1, NULL);
+INSERT INTO `columns` VALUES ('15302451002232884421', '课程资源', 1, NULL);
+INSERT INTO `columns` VALUES ('15302451002483359095', '课程建设', 1, NULL);
+INSERT INTO `columns` VALUES ('15302451002961331760', '问题与答疑', 1, NULL);
+INSERT INTO `columns` VALUES ('15302451003193761921', '课程研究', 1, NULL);
+INSERT INTO `columns` VALUES ('15302453257726227611', '课程简介', 2, '15302450997091385528');
+INSERT INTO `columns` VALUES ('15302453263166126949', '课程方法', 2, '15302450997091385528');
+INSERT INTO `columns` VALUES ('15302453263413343277', '课程安排', 2, '15302450997091385528');
+INSERT INTO `columns` VALUES ('15302453263681679501', '课程大纲', 2, '15302450997091385528');
+INSERT INTO `columns` VALUES ('15302453263914813765', '课程课程理念与目标', 2, '15302450997091385528');
+INSERT INTO `columns` VALUES ('15302453264073046748', '课程动态', 2, '15302450997091385528');
+INSERT INTO `columns` VALUES ('15302457338788694940', '教学课件', 2, '15302451002232884421');
+INSERT INTO `columns` VALUES ('15302457344044965038', '教学视频', 2, '15302451002232884421');
+
+-- ----------------------------
+-- Table structure for employee
+-- ----------------------------
+DROP TABLE IF EXISTS `employee`;
+CREATE TABLE `employee`  (
+  `id` char(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `name` char(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `sex` char(1) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `account` char(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `password` char(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `roleID` char(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of employee
+-- ----------------------------
+INSERT INTO `employee` VALUES ('15302466614265857775', 'yry', '男', 'superAdmin', 'admin', '15302463133562562266');
+
+-- ----------------------------
+-- Table structure for file
+-- ----------------------------
+DROP TABLE IF EXISTS `file`;
+CREATE TABLE `file`  (
+  `id` char(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `articleID` char(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `path` char(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for message
+-- ----------------------------
+DROP TABLE IF EXISTS `message`;
+CREATE TABLE `message`  (
+  `id` char(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `content` char(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `createTime` datetime(0) NULL DEFAULT NULL,
+  `reply` char(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `employeeID` char(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `replyTime` datetime(0) NULL DEFAULT NULL,
+  `status` int(11) NULL DEFAULT NULL,
+  `moduleID` char(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for module
+-- ----------------------------
+DROP TABLE IF EXISTS `module`;
+CREATE TABLE `module`  (
+  `id` char(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `name` char(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `status` int(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of module
+-- ----------------------------
+INSERT INTO `module` VALUES ('15302469445424745728', '通用网站', 1);
+
+-- ----------------------------
+-- Table structure for permission
+-- ----------------------------
+DROP TABLE IF EXISTS `permission`;
+CREATE TABLE `permission`  (
+  `id` char(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `moduleID` char(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `createTime` datetime(0) NULL DEFAULT NULL,
+  `description` char(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for role
+-- ----------------------------
+DROP TABLE IF EXISTS `role`;
+CREATE TABLE `role`  (
+  `id` char(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `name` char(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `createTime` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of role
+-- ----------------------------
+INSERT INTO `role` VALUES ('15302463133562562266', '超级管理员', '2018-06-29 12:25:13');
+
+-- ----------------------------
+-- Table structure for role_permission
+-- ----------------------------
+DROP TABLE IF EXISTS `role_permission`;
+CREATE TABLE `role_permission`  (
+  `id` char(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `roleID` char(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `permissionID` char(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for sonmodule
+-- ----------------------------
+DROP TABLE IF EXISTS `sonmodule`;
+CREATE TABLE `sonmodule`  (
+  `id` char(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `name` char(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `status` int(11) NULL DEFAULT NULL,
+  `parentID` char(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of sonmodule
+-- ----------------------------
+INSERT INTO `sonmodule` VALUES ('15302469451248343357', '系统设置', 1, '15302469445424745728');
+INSERT INTO `sonmodule` VALUES ('15302469451447462931', '网站内容管理', 1, '15302469445424745728');
+INSERT INTO `sonmodule` VALUES ('15302471607313394836', '留言管理', 1, '15302469451447462931');
+INSERT INTO `sonmodule` VALUES ('15302471612182192099', '文章管理', 1, '15302469451447462931');
+INSERT INTO `sonmodule` VALUES ('15302471612444683153', '审核文章', 1, '15302469451447462931');
+INSERT INTO `sonmodule` VALUES ('15302471612649526156', '栏目管理', 1, '15302469451447462931');
+INSERT INTO `sonmodule` VALUES ('15302473178576524100', '权限管理', 1, '15302469451248343357');
+INSERT INTO `sonmodule` VALUES ('15302473184721584464', '模块管理', 1, '15302469451248343357');
+INSERT INTO `sonmodule` VALUES ('15302473184917373766', '人员管理', 1, '15302469451248343357');
+INSERT INTO `sonmodule` VALUES ('15302473185171703745', '角色管理', 1, '15302469451248343357');
+INSERT INTO `sonmodule` VALUES ('15302473185354522409', '角色分配', 1, '15302469451248343357');
+
+SET FOREIGN_KEY_CHECKS = 1;
