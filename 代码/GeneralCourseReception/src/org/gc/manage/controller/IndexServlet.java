80eb8867a16eb335a3c25fbaeae874820cd81b47
xsq <1752475336@qq.com>
2018-07-02 17:15:29 +0800

    更新

---
+++ 代码/GeneralCourseReception/src/org/gc/manage/controller/IndexServlet.java
@@ -0,0 +1,15 @@
+package org.gc.manage.controller;
+
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+@WebServlet(name = "IndexServlet", urlPatterns = "/")
+public class IndexServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
